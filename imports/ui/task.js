0b04b41bace97a8ca32e5802c844811ac0dab3e1
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-01-08 13:54:30 +0000

    Add private button. Add private class to private tasks. Define helper to check ownership.

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -2,7 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
  
 import './task.html';
- 
+
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+
 Template.task.events({
   'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value

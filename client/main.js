6a75a1934298ca05ae1eb0b8668f3b76142a539d
Oladayo Osikoya <alwaysdeone@gmail.com>
2019-01-15 11:20:07 +0000

    Basic routing - display tasks only if user is logged in.

--- client/main.js
+++ client/main.js
@@ -1,2 +1,11 @@
-import '../imports/startup/accounts-config.js';
-import '../imports/ui/body.js';
+import { Template } from 'meteor/templating'
+
+import '../imports/startup/accounts-config.js'
+
+// Include routing
+import '../imports/startup/router.js'
+
+// Include pages
+import '../imports/ui/pages/landing.js'
+
+import './main.html'
---
+++ imports/startup/router.js
@@ -0,0 +1,9 @@
+// Landing pages
+
+FlowRouter.route('/', {
+  action: function() {
+    BlazeLayout.render('App_Body', {
+      main: 'Landing_page'
+    })
+  }
+})
